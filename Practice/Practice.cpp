63c9ceb1f0c6ab1f0e474587091c34802d5ad9f4
r_nagai@hip-pro.info <r_nagai@tr.hip-pro.net>
2025-01-22 15:14:22 +0900

    ファイル修正

--- Practice/Practice.cpp
+++ Practice/Practice.cpp
@@ -4,8 +4,8 @@
 
 int main(void) {
 	// 変数宣言
-	int g_player_num;  //プレイヤーの指定する数値
-	int g_cpu_num;  //CPUのランダムで指定する数値
+	int g_player_num = 0;  //プレイヤーの指定する数値
+	int g_cpu_num = 0;  //CPUのランダムで指定する数値
 	int i = 0; //プレイヤーの試行回数
 
 	// CPU側の乱数を設定
@@ -21,41 +21,37 @@ int main(void) {
 		printf("プレイヤーの入力値は：");
 		scanf_s("%d", &g_player_num);
 
-		// プレイヤーとCPUの数値の比較
-		// プレイヤーの数値がCPUの数値より大きい場合
-		if (g_player_num > g_cpu_num) {
-			// 入力値準正常ルート
-			if (g_player_num > 100) {
-				printf("NGな値が入力されています。\n");
-				printf("1～100の整数を入れましょうorz\n", g_player_num);
-			}
-			// 入力値正常ルート
-			else {
+		// 入力値が範囲外である時
+		if (g_player_num < 1 || g_player_num > 100) {
+			// NGメッセージの表示
+			printf("NGな値が入力されています。\n");
+			printf("1～100の整数を入れましょうorz\n");
+		}
+		// 入力値が1～100の整数である時
+		else {
+			// プレイヤーとCPUの数値の比較
+			// プレイヤーの数値がCPUの数値より大きい場合
+			if (g_player_num > g_cpu_num) {
 				// プレイヤーがCPUより大きい値である表示
 				printf("Big\n");
 				printf("%dより小さい値を入れましょうorz\n", g_player_num);
 			}
-		}
-		// プレイヤーの数値がCPUの数値より小さい場合
-		else if (g_player_num < g_cpu_num) {
-			// プレイヤーがCPUより小さい値の際の不正パターンのあぶり出し
-			// 入力値準正常ルート
-			if (g_player_num < 0) {
-				printf("NGな値が入力されています。\n");
-				printf("1～100の整数を入れましょうorz\n", g_player_num);
-			}
-			// 入力値正常ルート
-			else {
+			// プレイヤーの数値がCPUの数値より小さい場合
+			else if (g_player_num < g_cpu_num) {
 				// プレイヤーがCPUより小さい値である表示
 				printf("Small\n");
 				printf("%dより大きい値を入れましょうorz\n", g_player_num);
 			}
+			// プレイヤーが一致する数値をあてた場合
+			else {
+				// プレイヤーが一致する数値をあてた際の表示
+				printf("Bingo!\n");
+			}
+
 		}
 		i++;
 	} while (g_player_num != g_cpu_num);
 
-	// プレイヤーが一致する数値をあてた際の表示
-	printf("Bingo!\n");
 	// プレイヤーの試行回数を表示
 	printf("プレイヤーは%d回で当てることができました。\n", i);
 	return 0;
