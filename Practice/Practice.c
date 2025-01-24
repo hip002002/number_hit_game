e42788b7ce158483e44557f0761a42f328aa7a58
r_nagai@hip-pro.info <r_nagai@tr.hip-pro.net>
2025-01-24 16:02:30 +0900

    再再再レビュー用w

--- Practice/Practice.c
+++ Practice/Practice.c
@@ -2,50 +2,52 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdbool.h>
+#define ANSWER_MAX_NUM 100
+#define ANSWER_MIN_NUM 1
 
 int main(void) {
-	int input_num = 0;
+	char input_string[100];
+	long input_long_num = 0;
+	int input_int_num = 0;
 	int answer_num = 0;
 	int trial_num = 0;
-	int max_num = 100;
-	int min_num = 1;
-	bool loop_escape_result = false;
-	char irregular_message[100] = "NGな値が入力されています。";
-	char compare_big_message[100] = "Big";
-	char compare_small_message[100] = "Small";
-	char compare_bingo_message[100] = "Bingo!";
-	char input_message[100] = "プレイヤーの入力値は：";
+	bool bingo_result = false;
+	char* input_string_endptr;
 
 	srand((unsigned int)time(NULL));
-	answer_num = (rand() % max_num) + min_num;
+	answer_num = (rand() % (ANSWER_MAX_NUM -ANSWER_MIN_NUM +1)) + ANSWER_MIN_NUM;
 
-	printf("%d～%dまでの整数を入力してください!\n", min_num, max_num);
+	printf("%d～%dまでの整数を入力してください!\n", ANSWER_MIN_NUM, ANSWER_MAX_NUM);
 
-	do {
-		printf("%s", input_message);
-		scanf_s("%d", &input_num);
-
-		if ((input_num < min_num) || (input_num > max_num)) {
-			printf("%s\n", irregular_message);
-			printf("%d～%dの整数を入れましょう\n", min_num, max_num);
-		}
-		else {
-			if (input_num > answer_num) {
-				printf("%s\n", compare_big_message);
-				printf("%dより小さい整数を入れましょう\n", input_num);
+	while (bingo_result == false) {
+		trial_num++;
+		printf("プレイヤーの入力値は：");
+		fgets(input_string, sizeof(input_string), stdin);
+		input_string[strcspn(input_string, "\n")] = '\0';
+		input_long_num = strtol(input_string, &input_string_endptr, 10);
+		input_int_num = input_long_num;
+		if (*input_string_endptr == '\0' && input_string[0] != '\0') {
+			if ((input_int_num < ANSWER_MIN_NUM) || (input_int_num > ANSWER_MAX_NUM)) {
+				printf("NGな値が入力されています。\n");
+				printf("%d～%dの整数を入れましょう\n", ANSWER_MIN_NUM, ANSWER_MAX_NUM);
 			}
-			else if (input_num < answer_num) {
-				printf("%s\n", compare_small_message);
-				printf("%dより大きい整数を入れましょう\n", input_num);
+			else if (input_int_num > answer_num) {
+				printf("Big\n");
+				printf("%dより小さい整数を入れましょう\n", input_int_num);
+			}
+			else if (input_int_num < answer_num) {
+				printf("Small\n");
+				printf("%dより大きい整数を入れましょう\n", input_int_num);
 			}
 			else {
-				printf("%s\n", compare_bingo_message);
-				loop_escape_result = true;
+				printf("Bingo!\n");
+				bingo_result = true;
 			}
 		}
-		trial_num++;
-	} while (loop_escape_result = false);
-
+		else {
+			printf("数値のみの入力がされてません。数値を入力してください。\n");
+		}
+	} 
 	printf("プレイヤーは%d回で当てることができました。\n", trial_num);
 	return 0;
 }
